68ed16c80c723cd52f8c39a01755c19b21ea4203
GBShadow <gbs.shadow@gmail.com>
2020-08-14 12:03:54 -0300

    Create landing page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 export default function App() {
-  return (
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
 
